a0394cdff21db6a460989942041d5070c44bdba8
kais-chalghoumi <kais.chalghoumi@esprit.tn>
2023-02-23 11:27:37 +0100

    S 5

--- KaisAngularProject/src/app/Services/product.service.ts
+++ KaisAngularProject/src/app/Services/product.service.ts
@@ -7,9 +7,9 @@ import { Product } from '../core/model/Product';
 export class ProductService {
 
   listProduct : Product[] = [
-  {id: 1, title: "T-shirt 1", price: 18, quantity: 0, like: 0},
-  {id: 2, title: "T-shirt 2", price: 21, quantity: 10, like: 0},
-  {id: 3, title: "T-shirt 3", price: 16, quantity: 8, like: 0},
+  {id: 0, title: "T-shirt 1", price: 18, quantity: 0, like: 0},
+  {id: 1, title: "T-shirt 2", price: 21, quantity: 10, like: 0},
+  {id: 2, title: "T-shirt 3", price: 16, quantity: 8, like: 0},
 ];
   constructor() { }
   addProduct(product : Product){
--- KaisAngularProject/src/app/detail-product/detail-product.component.ts
+++ KaisAngularProject/src/app/detail-product/detail-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from '../core/model/Product';
+import { ProductService } from '../Services/product.service';
 
 @Component({
   selector: 'app-detail-product',
@@ -8,11 +10,13 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailProductComponent implements OnInit {
 
+  product!: Product;
   id!:number;
-  constructor(private route : ActivatedRoute) { }
+  constructor(private productService : ProductService,private route : ActivatedRoute) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+    this.product = this.productService.listProduct[this.id];
   }
 
 }
